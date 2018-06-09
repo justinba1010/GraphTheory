2e798fc6f2f4167baa3c2c9f3db29fe5ff66061b
Justin Baum <justinba1010@gmail.com>
2018-06-09 02:06:34 -0400

    Greedy Coloring Algorithm + Random Greedy Coloring Algorithm

--- AdjNodeMatrixGraph.java
+++ AdjNodeMatrixGraph.java
@@ -17,18 +17,46 @@ public class AdjNodeMatrixGraph<T extends Node> extends AdjMatrixGraph<T> {
   }
 
   public void ColorGraphGreedy() {
+    //Color all nodes -1
+    for(T node : this.nodes) {
+      node.setColor(-1);
+    }
+    //Greedy Algorithm
     for(T node : this.nodes) {
       ArrayList<T> adjacents = this.getAdjacent(node);
-      int color = 0;
+      int color = -1;
       boolean matched = true;
       while(matched) {
+        color++;
         matched = false;
         for(T adjacentNode : adjacents) {
-          if(color == adjacentNode.getColor()) {
-            matched = true;
-            color++;
-            break;
-          }//if
+          matched |= adjacentNode.getColor() == color;
+        }//for neighbor
+      }//while
+      node.setColor(color);
+    }//for
+  }//ColorGraphGreedy
+  public void ColorGraphGreedyRandom() {
+    //Color all nodes -1
+    for(T node : this.nodes) {
+      node.setColor(-1);
+    }
+    //Randomize list
+    ArrayList<T> randomOrderedNodes = new ArrayList<T>();
+    for(T node : this.nodes) {
+      randomOrderedNodes.add(node);
+    }
+    Collections.shuffle(randomOrderedNodes);
+    //Greedy Algorithm
+    for(T node : randomOrderedNodes) {
+      ArrayList<T> adjacents = this.getAdjacent(node);
+      int color = -1;
+      boolean matched = true;
+      while(matched) {
+        color++;
+        matched = false;
+        for(T adjacentNode : adjacents) {
+          matched |= adjacentNode.getColor() == color;
         }//for neighbor
       }//while
       node.setColor(color);
--- AdjNodeMatrixGraphDriver.java
+++ AdjNodeMatrixGraphDriver.java
@@ -7,10 +7,13 @@ public class AdjNodeMatrixGraphDriver {
     l.add(u1);
     l.add(u2);
     AdjNodeMatrixGraph aGraph = new AdjNodeMatrixGraph(l);
+    aGraph.addEdge(0,1);
     Node u3 = (Node)aGraph.nodes.get(0);
     System.out.println(u3.getColor());
     aGraph.ColorGraphGreedy();
     u3 = (Node)aGraph.nodes.get(0);
+    System.out.println(u3.getColor());
+    u3 = (Node)aGraph.nodes.get(1);
     System.out.println(u3.getColor());
     //For debugging
     //Looks like it is working how I want it to.
